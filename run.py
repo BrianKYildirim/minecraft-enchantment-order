1b4f0220afe278fa5eae3fae3261cba1eb65ca21
Brian Yildirim <byildiri@uci.edu>
2025-05-19 04:34:29 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-from app import create_app
+from app import create_app, send_from_directory
 
 app = create_app()
 
+@app.route("/ads.txt")
+def ads_txt():
+    return send_from_directory(app.root_path, "ads.txt", mimetype="text/plain")
+
 if __name__ == "__main__":
-    # For local development
     app.run(host="0.0.0.0", debug=True)
