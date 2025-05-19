e9eab8e74d3d213429d523cf273d4e53c086c96a
Brian Yildirim <byildiri@uci.edu>
2025-05-19 04:33:07 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -61,10 +61,3 @@ def calculate():
 
     except (ValueError, IncompatibleSelected, MergeTooExpensive, RuntimeError) as e:
         return str(e), 400
-
-@main.route("/ads.txt")
-def ads_txt():
-    return send_from_directory(
-        current_app.root_path,
-        "ads.txt",
-        mimetype="text/plain")
